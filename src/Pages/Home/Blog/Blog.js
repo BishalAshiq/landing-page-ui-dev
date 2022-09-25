69593d5cbb48794948c0fd5261ee9f23a50bc980
BishalAshiq <bishaltheboss20@gmail.com>
2022-09-26 02:35:09 +0600

    added more Items

---
+++ src/Pages/Home/Blog/Blog.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Swiper from 'swiper';
+import 'swiper/css';
+
+const Blog = () => {
+    return (
+        <div class="swiper">
+  <div class="swiper-wrapper">
+    <div class="swiper-slide">Slide 1</div>
+    <div class="swiper-slide">Slide 2</div>
+    <div class="swiper-slide">Slide 3</div>
+    ...
+  </div>
+</div>
+    );
+};
+
+export default Blog;
